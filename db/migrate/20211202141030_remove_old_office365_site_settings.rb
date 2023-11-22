91d46e53cb76463abab96574cb9e30506c40d594
Albin Antony <albinantony.2904@gmail.com>
2023-11-22 19:39:41 +0530

    Update 20211202141030_remove_old_office365_site_settings.rb

--- db/migrate/20211202141030_remove_old_office365_site_settings.rb
+++ db/migrate/20211202141030_remove_old_office365_site_settings.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class RemoveOldOffice365SiteSettings < ActiveRecord::Migration[6.1]
   def up
-    execute "DELETE FROM site_settings WHERE name IN ('office365_enabled', 'office365_client_id', 'office365_secret')"
+    execute "DELETE FROM site_settings WHERE name IN ('office365_enabled', 'office365_client_id', 'office365_secret', 'office365_tenant_id')"
   end
 
   def down
